f98da7492c2957a37f3e9260c55968e88fd3e6e9
Viola <viola-malt-0s@icloud.com>
2026-08-27 15:43:16 +0200

    Stage 29: Добавь конфигурацию приложения через словарь настроек

---
+++ travel_ledger_part29.py
@@ -0,0 +1,15 @@
+# === Stage 29: Добавь конфигурацию приложения через словарь настроек ===
+# Project: TravelLedger
+APP_CONFIG = {
+    "app_name": "TravelLedger",
+    "version": "1.0",
+    "currency": "USD",
+    "languages": ["en", "ru"],
+    "max_routes": 10,
+    "max_bookings": 50,
+    "max_documents": 20,
+    "max_budgets": 5,
+    "log_level": "INFO",
+    "date_format": "%Y-%m-%d",
+    "budget_threshold_pct": 80,
+}
